a42dd732fbb5f5e7e37a0c92f7cce1a65f9d0413
Conco <cmaderblanco@gmail.com>
2024-04-23 14:50:43 -0300

    typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import MegaverseMap from "./components/MegaverseMap"
+
+function App() {
+  return (
+    <div className="appBody">
+      <h1>Your Megaverse</h1>
+      <MegaverseMap />
+    </div>
+  );
+}
+
+export default App;
